cfe61b7c856a185948462437e1362ae1e8a98263
F.P.P <kelly1999010212@gmail.com>
2019-11-07 14:42:53 +0800

    E1080305

---
+++ UnityProject/Assets/GC.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GC : MonoBehaviour
+{
+
+    public AudioMixer mixer;
+
+    public Text loadingText;
+    public Slider loading;
+
+    public void SetVSFX(float value)
+    {
+        mixer.SetFloat("VSFX", value);
+    }
+
+
+}
